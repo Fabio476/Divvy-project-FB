c7592b92a06342aa7b76bb01e4a1bccfb72845b2
Fabio476 <140005952+Fabio476@users.noreply.github.com>
2023-07-19 13:45:10 -0500

    Update project.R

--- project.R
+++ project.R
@@ -2,12 +2,14 @@ library(tidyverse)
 library(lubridate)
 library(ggplot2)
 library(scales)
-## Downloads the CVS file into R and assings them a variable name to later bind.
+
+## Downloads the CVS file into R and assigns them a variable name to later bind.
 q2_2019 <- read_csv("Divvy_Trips_2019_Q2.csv")
 q3_2019 <- read_csv("Divvy_Trips_2019_Q3.csv")
 q4_2019 <- read_csv("Divvy_Trips_2019_Q4.csv")
 q1_2020 <- read_csv("Divvy_Trips_2020_Q1.csv")
-## This rewrites all the files to have the same column name. This way all collumn
+
+## This rewrites all the files to have the same column name. This way all column
 ## will bind easily.
 (q4_2019 <- rename(q4_2019
                    ,ride_id = trip_id
@@ -46,35 +48,41 @@ q3_2019 <-  mutate(q3_2019, ride_id = as.character(ride_id)
                    ,rideable_type = as.character(rideable_type)) 
 q2_2019 <-  mutate(q2_2019, ride_id = as.character(ride_id)
                    ,rideable_type = as.character(rideable_type)) 
+
 ##Binds all files into one and 
 all_trips <- bind_rows(q2_2019, q3_2019, q4_2019, q1_2020)
-##Romoves all columns that are not needed for this analasys
+
+##Romoves all columns that are not needed for this analysis
 all_trips <- all_trips %>%  
   select(-c(start_lat, start_lng, end_lat, end_lng, birthyear, gender, "01 - Rental Details Duration In Seconds Uncapped", "05 - Member Details Member Birthday Year", "Member Gender", "tripduration"))
 
-## there are 4 types of members since there was a change
+## There are 4 types of members since there was a change
 all_trips <-  all_trips %>% 
   mutate(member_casual = recode(member_casual
                                 ,"Subscriber" = "member"
                                 ,"Customer" = "casual"))
+
 ##This created a new variable for the date. This variable is then used to 
-##to create multiple date variable. Each variable corresponds to month, day,year, and day of week
+##to create multiple date variables. Each variable corresponds to the month, day, year, and day of the week
 
 all_trips$date <- as.Date(all_trips$started_at)
 all_trips$month <- format(as.Date(all_trips$date), "%m")
 all_trips$day <- format(as.Date(all_trips$date), "%d")
 all_trips$year <- format(as.Date(all_trips$date), "%Y")
 all_trips$day_of_week <- format(as.Date(all_trips$date), "%A")
-## this creates a variable that looks at the time difference between 2 times(last-first)
+
+## This creates a variable that looks at the time difference between 2 times(last-first)
 all_trips$ride_length <- difftime(all_trips$ended_at,all_trips$started_at)
 
 is.factor(all_trips$ride_length)
 all_trips$ride_length <- as.numeric(as.character(all_trips$ride_length))
 is.numeric(all_trips$ride_length)
-##Removes bad data: When the bikes where in the shop and anytime that is negative
+
+##Removes bad data: When the bikes were in the shop and anytime that is negative
 all_trips_v2 <- all_trips[!(all_trips$start_station_name == "HQ QR" | all_trips$ride_length<0),]
-## This codes looks at the mean, median, max, and min of ride length. 
-## This is just to get a quick grasp of how on how the data looks.
+
+## This code looks at the mean, median, max, and min of ride length. 
+## This is just to get a quick grasp of how the data looks.
 mean(all_trips_v2$ride_length)
 median(all_trips_v2$ride_length) 
 max(all_trips_v2$ride_length) 
@@ -84,11 +92,13 @@ aggregate(all_trips_v2$ride_length ~ all_trips_v2$member_casual, FUN = mean)
 aggregate(all_trips_v2$ride_length ~ all_trips_v2$member_casual, FUN = median)
 aggregate(all_trips_v2$ride_length ~ all_trips_v2$member_casual, FUN = max)
 aggregate(all_trips_v2$ride_length ~ all_trips_v2$member_casual, FUN = min)
-## This changes the day of week variable that is in. order.
+
+## This changes the day of the week variable that is in. order.
 all_trips_v2$day_of_week <- ordered(all_trips_v2$day_of_week, levels=c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"))
 
 aggregate(all_trips_v2$ride_length ~ all_trips_v2$member_casual + all_trips_v2$day_of_week, FUN = mean)
-## This creates a table that compares the number of rides to the day of week for 
+
+## This creates a table that compares the number of rides to the day of the week for 
 ##casual and member. This is to understand how the usage looks over the week.
 all_trips_v2 %>% 
   mutate(weekday = wday(started_at, label = TRUE)) %>% 
@@ -99,6 +109,7 @@ all_trips_v2 %>%
   ggplot(aes(x = weekday, y = number_of_rides, fill = member_casual)) +
   geom_col(position = "dodge")+
   scale_y_continuous(labels = comma)
+
 ##This creates a table that compares the length of ride per member. This is to 
 ##understand how the member is using the bike.
 all_trips_v2 %>% 
@@ -110,8 +121,9 @@ all_trips_v2 %>%
   ggplot(aes(x = weekday, y = average_duration, fill = member_casual)) +
   geom_col(position = "dodge")+
   scale_y_continuous(labels = comma)
-##This creates a table that looks at the amount of rides per month fo both members.
-## This is table will help us understand the trend between the month and the amount of members.
+
+##This creates a table that looks at the number of rides per month for both members.
+## This table will help us understand the trend between the month and the number of members.
 all_trips_v2 %>% 
   ggplot(aes(x= month, fill = member_casual))+
   geom_bar(position = "dodge")+
